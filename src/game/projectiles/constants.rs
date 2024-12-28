55443c681022497abb1021ba0f228474536bd2df
Michal Skrzypiec <skrzypiecmi@gmail.com>
2024-12-28 12:33:06 +0100

    added few initial elements

--- src/game/projectiles/constants.rs
+++ src/game/projectiles/constants.rs
@@ -1,7 +1,11 @@
 pub const CAST_SPEED: f32 = 3.95; // casts per seconds
 pub const SPEED: f32 = 500.;
-pub const TEXTURE_PATH: &str = "BlackCircle50x50.png";
+pub const PROJECTILE_TEXTURE_PATH: &str = "BlackCircle50x50.png";
 pub const SPRITE_DIAMETER: f32 = 10.;
 pub const SPRITE_DEPTH: f32 = 30.;
 pub const DURATION: f32 = 2.;
 pub const DAMAGE: f32 = 50.;
+pub const EXPLOSION_TEXTURE_PATH: &str = "ExplosionAtlas200x200x3x1.png";
+pub const EXPLOSION_TEXTURE_SIZE: u32 = 200;
+pub const EXPLOSION_TEXTURE_COLUMNS: u32 = 3;
+pub const EXPLOSION_TEXTURE_ROWS: u32 = 1;
