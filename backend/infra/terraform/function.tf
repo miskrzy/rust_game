bb2907e94ca8ceab984efb98ef83e13ec98230e7
Michal Skrzypiec <skrzypiecmi@gmail.com>
2025-02-09 19:57:42 +0100

    adjusted to service plan

--- backend/infra/terraform/function.tf
+++ backend/infra/terraform/function.tf
@@ -1,21 +1,16 @@
-resource "azurerm_app_service_plan" "this" {
+resource "azurerm_service_plan" "this" {
   name                = format("%s-appserviceplan", local.prefix)
   location            = data.azurerm_resource_group.this.location
   resource_group_name = data.azurerm_resource_group.this.name
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+  os_type             = "Linux"
+  sku_name            = "Y1"
 }
 
 resource "azurerm_function_app" "this" {
   name                       = format("%s-function", local.prefix)
   location                   = data.azurerm_resource_group.this.location
   resource_group_name        = data.azurerm_resource_group.this.name
-  app_service_plan_id        = azurerm_app_service_plan.this.id
+  app_service_plan_id        = azurerm_service_plan.this.id
   storage_account_name       = azurerm_storage_account.this.name
   storage_account_access_key = azurerm_storage_account.this.primary_access_key
   os_type                    = "linux"
