964fd4558bdec79f37a8de7b1b1db2539db18c65
Michał Skrzypiec <michal.skrzypiec@unit8.co>
2025-11-16 16:23:43 +0100

    moved to flex consumption plan

--- backend/infra/terraform/function.tf
+++ backend/infra/terraform/function.tf
@@ -7,24 +7,26 @@ resource "azurerm_service_plan" "this" {
 }
 
 
-resource "azurerm_linux_function_app" "this" {
+resource "azurerm_function_app_flex_consumption" "this" {
   name                = format("%s-function", local.prefix)
   resource_group_name = data.azurerm_resource_group.this.name
   location            = data.azurerm_resource_group.this.location
+  service_plan_id     = azurerm_service_plan.this.id
 
-  storage_account_name       = azurerm_storage_account.this.name
-  storage_account_access_key = azurerm_storage_account.this.primary_access_key
-  service_plan_id            = azurerm_service_plan.this.id
+  storage_container_type = "blobContainer"
 
-  # https_only      = true
+  storage_container_endpoint  = "${azurerm_storage_account.this.primary_blob_endpoint}${azurerm_storage_container.this.name}"
+  storage_authentication_type = "StorageAccountConnectionString"
+  storage_access_key          = azurerm_storage_account.this.primary_access_key
+  runtime_name                = "python"
+  runtime_version             = "3.12"
+  instance_memory_in_mb       = 512
+
   zip_deploy_file = data.archive_file.this.output_path
 
-  site_config {
+  app_settings = {
     application_insights_connection_string = azurerm_application_insights.this.connection_string
   }
 
-  app_settings = {
-    FUNCTIONS_WORKER_RUNTIME = "python"
-  }
+  site_config {}
 }
-
--- backend/infra/terraform/storage.tf
+++ backend/infra/terraform/storage.tf
@@ -7,3 +7,10 @@ resource "azurerm_storage_account" "this" {
   account_replication_type  = "LRS"
   shared_access_key_enabled = true
 }
+
+
+resource "azurerm_storage_container" "this" {
+  name                  = format("%sfunccontainer", local.prefix)
+  storage_account_id    = azurerm_storage_account.this.id
+  container_access_type = "private"
+}
