efea4ebf8d838a6c7b9486e86a30e6c02b5ad3bf
Michał Skrzypiec <michal.skrzypiec@unit8.co>
2025-11-16 17:40:31 +0100

    added template for function deployment pipeline

--- backend/infra/terraform/function.tf
+++ backend/infra/terraform/function.tf
@@ -29,16 +29,3 @@ resource "azurerm_function_app_flex_consumption" "this" {
   site_config {}
 }
 
-
-# resource "time_sleep" "wait_for_function_app" {
-#   depends_on      = [azurerm_function_app_flex_consumption.this]
-#   create_duration = "30s"
-# }
-
-
-# resource "azurerm_function_app_deployment" "this" {
-#   depends_on      = [time_sleep.wait_for_function_app]
-#   function_app_id = azurerm_function_app_flex_consumption.this.id
-#   filename        = data.archive_file.this.output_path
-#   build_remote    = false
-# }
